9b335512f8c765a5753867b9a8092ed0ee36a433
Pradeep Yadav <pradeepkyadav284@gmail.com>
2024-12-18 18:01:28 +0530

    INIT

---
+++ hello_lund.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main () {
+    printf("hello_i_am_pradeep_the_small_dick");
+    return 0;
+}
---
+++ utsav_baap_pradeep_ka.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main () {
+    for(int i = 0; i < 22 ; i++){
+        printf("hello_i_am_pradeep_the_small_dick\n");
+    }
+
+    return 0;
+}
